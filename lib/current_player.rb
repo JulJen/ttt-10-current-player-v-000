4f8e50bee1adfb7fcb5aa23322e49984579752a5
Julia J-Prog <juliajen.prog@gmail.com>
2017-10-20 22:23:03 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,21 +1,22 @@
-# turn_count method takes in an argument of the board array and returns the number of turns that have been played.
 
-def turn_count(board)
-  count = 0
-  board.each do |b|
-    if b == "X" || b == "O"
-      count += 1
+def turn_count(board) # Takes in an argument of the board array
+  count = 0 # Variable keeps track of number of turns
+  board.each do |b|  # Iterates over each member of the board array
+    if b == "X" || b == "O" # checks to see if that element is an "X" or an "O"
+      count += 1 # If true, increments our counter variable by 1.
     end
   end
-  count
+  count # Returns the number of turns that have been played.
 end
 
- #turn_count method to determine if it is "X"'s turn or "O"'s. If the turn count is an even number, the #current_player method returns "X", otherwise, returns "O".
 
-def current_player(board) #current_player method takes in an argument of the game board
-  turn_count(board).even? ? board = "X" : board = "O"
+
+# Turn_count method to determine if it is "X"'s turn or "O"'s
+
+def current_player(board) # Current_player method takes in an argument of the game board
+  turn_count(board).even? ? board = "X" : board = "O" 
 end
-
+# If the turn count is an even number, the #current_player method returns "X", otherwise, returns "O"
 
 # def current_player(board)
 #   if turn_count(board).even?
