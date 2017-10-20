85f4a5b5b98b20daafefa24a1cd0d06def70ae73
Julia J-Prog <juliajen.prog@gmail.com>
2017-10-20 22:13:44 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,17 +12,14 @@ end
 
 and uses the #turn_count method to determine if it is "X"'s turn or "O"'s. If the turn count is an even number, the #current_player method returns "X", otherwise, returns "O".
 
-def current_player(board) #current_player method takes in an argument of the game board 
-  if turn_count(board).even? #block method determines if "X" "
-    board = "X"
-  else
-    board = "O"
+def current_player(board) #current_player method takes in an argument of the game board
+  turn_count(board).even? ? board = "X" : board = "O"
   end
 end
 
 
 # def current_player(board)
-#   if turn_count(board).even? 
+#   if turn_count(board).even?
 #     board = "X"
 #   else
 #     board = "O"
